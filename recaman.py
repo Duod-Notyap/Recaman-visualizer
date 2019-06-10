03676cf7beaa1bc42fd7522e6c4dafe5524535f3
Duod-Notyap <35939909+Duod-Notyap@users.noreply.github.com>
2019-06-10 16:28:31 -0700

    Update recaman.py

--- recaman.py
+++ recaman.py
@@ -1,15 +1,16 @@
 import time
-from turtle import *
+import turtle
 import math
 placesBeen=[1]
 i = 0
 c = 0
-t = Turtle()
+t = turtle.Turtle()
 t.pu()
 t.setpos(-955, 0)
 t.pd()
 t.seth(90)
 t.speed(0)
+turtle.tracer(0, 0)
 def drawSemi(r, l, n):
     k = l-r
     m=2.3
@@ -29,6 +30,8 @@ def drawSemi(r, l, n):
         for f in range(0, 180):
             t.forward((math.pi*math.fabs(k))*m/360)
             t.right(1)
+            
+            
 while True:
     i += 1
     if c-i in placesBeen or c-i <= 0:
@@ -41,3 +44,4 @@ while True:
         print("Back {}".format(c))
         placesBeen.append(c)
         drawSemi(c+i, c, (-1)**(i+1))
+    turtle.update()
